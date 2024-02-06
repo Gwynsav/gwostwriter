29adf91541475ad35b950860cf9c5e6be7fcd9ab
Gwynsav <n0n3ofyourbus1nes5@outlook.com>
2024-02-06 12:27:20 +0100

    Added LaTeX sources, removed/rebound annoying cmp keybindings

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -26,10 +26,15 @@ return {
 	{
 		'hrsh7th/nvim-cmp',
 		dependencies = {
+         -- Snippets.
 			'L3MON4D3/LuaSnip',
 			'saadparwaiz1/cmp_luasnip',
 			'rafamadriz/friendly-snippets',
-			'hrsh7th/cmp-nvim-lsp'
+			'hrsh7th/cmp-nvim-lsp',
+         -- LaTeX.
+         'lervag/vimtex',
+         'micangl/cmp-vimtex',
+         'kdheepak/cmp-latex-symbols'
 		},
       lazy = true
 	},
