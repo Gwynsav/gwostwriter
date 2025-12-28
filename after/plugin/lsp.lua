7876ec136a7464480a476cecbd6f5abf06883d30
Gwynsav <n0n3ofyourbus1nes5@outlook.com>
2025-12-28 22:52:38 +0100

    Fix broken Mason config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,57 +1,47 @@
 -- nvim/after/plugin/lsp.lua
 
--- Sets LSP keybinds up.
-local on_attach = function(_, bufnr)
-	local bufmap = function(keys, func)
-		vim.keymap.set('n', keys, func, { buffer = bufnr })
-	end
-
-	bufmap('<leader>r', vim.lsp.buf.rename)
-	bufmap('<leader>a', vim.lsp.buf.code_action)
-	bufmap('gd', vim.lsp.buf.definition)
-	bufmap('gD', vim.lsp.buf.declaration)
-	bufmap('gI', vim.lsp.buf.implementation)
-	bufmap('<leader>D', vim.lsp.buf.type_definition)
-
-	-- Telescope LSP keybinds.
-	bufmap('gr', require('telescope.builtin').lsp_references)
-	bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
-	bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
-
-	bufmap('K', vim.lsp.buf.hover)
-
-	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-		vim.lsp.buf.format()
-	end, {})
-end
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
--- Sets the language server installer up.
-require('mason').setup()
+--- Configure Mason.
+require('mason').setup({
+   ui = {
+      icons = {
+         package_installed   = '[I]',
+         package_pending     = '[P]',
+         package_uninstalled = '[X]'
+      }
+   }
+})
 require('mason-lspconfig').setup({
-	-- General handler.
-	function(server_name)
-		require('lspconfig')[server_name].setup({
-			on_attach = on_attach,
-			capabilities = capabilities
-		})
-	end,
-	-- Lua handler.
-	['lua_ls'] = function()
-		-- Prevent LSP from marking "vim" as an undefined global.
-		require('neodev').setup()
-		require('lspconfig').lua_ls.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			Lua = {
-				workspace = { checkThirdParty = false },
-				telemetry = { enable = false },
-            diagnostics = {
-               globals = { 'vim', 'awesome', 'client', 'screen', 'mouse', 'tag' }
-            }
-			}
-		})
-	end
+   ensure_installed = { 'lua_ls' }
+})
+
+-- Language Servers
+-------------------
+--- General.
+vim.api.nvim_create_autocmd('LspAttach', {
+   callback = function(args)
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if not client then return end
+
+      -- Default mappings:
+      --    `gra` brings up code actions.      `gri` jumps to function implementation.
+      --    `grn` renames a symbol.            `grr` lists references to a symbol.
+      --    `grt` jumps to type definition.    `K`   shows symbol information.
+      --    `i_CTRL-X_CTRL-O` shows completion window.
+      -- XXX: Add new stuff here...
+   end
+})
+--- Lua.
+vim.lsp.enable('lua_ls')
+vim.lsp.config('lua_ls', {
+   filetypes = { 'lua' },
+   settings = {
+      Lua = {
+         runtime = { version = 'LuaJIT' },
+			workspace = { checkThirdParty = false },
+			telemetry = { enable = false },
+         diagnostics = {
+            globals = { 'vim', 'awesome', 'client', 'screen', 'mouse', 'tag' }
+         }
+      }
+   }
 })
